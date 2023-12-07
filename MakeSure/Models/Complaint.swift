50b8d555b23c7e7d5db7a83caa8f8592e69bf612
andreydem <andreydem906.082@gmail.com>
2023-12-07 18:38:07 +0300

    Implemented uploading images, changed design

---
+++ MakeSure/Models/Complaint.swift
@@ -0,0 +1,8 @@
+//
+//  Complaint.swift
+//  MakeSure
+//
+//  Created by Macbook Pro on 06.12.2023.
+//
+
+import Foundation
---
+++ MakeSure/Views/Utils/BackspaceDetectingTextField.swift
@@ -0,0 +1,8 @@
+//
+//  BackspaceDetectingTextField.swift
+//  MakeSure
+//
+//  Created by Macbook Pro on 07.12.2023.
+//
+
+import Foundation
